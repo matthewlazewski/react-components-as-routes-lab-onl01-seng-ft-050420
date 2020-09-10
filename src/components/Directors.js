ee89cebd81e478f8c72441fb42738ed024b60262
matthewlazewski <matthew.lazewski@gmail.com>
2020-09-10 09:49:51 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+    
+    return (
+        <div>
+            <h1>Directors Page</h1>
+            {directors.map((director) => 
+                <div>
+                    <h2 key={director.name}>Name: {director.name}</h2>
+                    <p>Movies: </p>
+                    <ul>
+                        {director.movies.map((movie,index) => 
+                        <li key={index}>{movie}</li>
+                        )}
+                    </ul>
+                </div>
+            )}
+        </div>
+    )
 }
 
-export default Directors
+export default Directors;
